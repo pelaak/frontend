84ff2192b34aa71ff0ea6b7558cd799ca3bcb09b
hossen <iamhoosyn@gmail.com>
2024-11-12 21:05:13 +0330

    responsive home page

--- pelak-front/src/Pages/HomePage.js
+++ pelak-front/src/Pages/HomePage.js
@@ -11,6 +11,18 @@ function Home() {
     { name: 'عبداله باقری', image: image_address1 },
     { name: 'ابراهیم هادی', image: image_address2 },
     { name: 'قاسم سلیمانی', image: image_address3 },
+    { name: 'عبداله باقری', image: image_address1 },
+    { name: 'ابراهیم هادی', image: image_address2 },
+    { name: 'قاسم سلیمانی', image: image_address3 },
+    { name: 'عبداله باقری', image: image_address1 },
+    { name: 'ابراهیم هادی', image: image_address2 },
+    { name: 'قاسم سلیمانی', image: image_address3 },
+    { name: 'عبداله باقری', image: image_address1 },
+    { name: 'ابراهیم هادی', image: image_address2 },
+    { name: 'قاسم سلیمانی', image: image_address3 },
+    { name: 'عبداله باقری', image: image_address1 },
+    { name: 'ابراهیم هادی', image: image_address2 },
+    { name: 'قاسم سلیمانی', image: image_address3 },
   ];
 
   return (
@@ -28,13 +40,13 @@ function Home() {
                             <a className="nav-link" href="#">خانه</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">درباره ما</a>
+                            <a className="nav-link" href="#">ارتباط با ما</a>
                         </li>
                     </ul>
                 </div>
                 <a className="navbar-brand" href="#">
                     <div className="logo-container">
-                        <img src="https://static.heyat.co/files/upload/disks_pre-images/ramezan/disk/heyat.co-nazr-mandegar-3-1.jpg" alt="لوگو راست" style={{ width: '70px', borderRadius: "5px" }} />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1iXJSICqCgserhWeGFVhh2Ec085BQZBXLiNsbekoXgGxudjPJ" alt="لوگو راست" style={{ width: '70px', borderRadius: "5px" }} />
                     </div>
                 </a>
             </div>
@@ -45,14 +57,14 @@ function Home() {
         </header>
         <div className="container">
             <div className="card-container p-4 rounded">
-                <div className="row">
+                <div className="row justify-content-center">
                     {people.map((person, index) => (
-                        <div className="col-6 mb-4" key={index}>
+                        <div className="col-md-3 col-6 mb-4" key={index}>
                             <div className="card text-center bg-light text-dark card-hover oval-card p-1">
                                 <img src={person.image} alt={person.name} className="card-img-top oval-image m-0" />
                                 <div className="card-body">
-                                    <div><span style={{color: "red", fontSize: "10px"}}>شهید والامقام</span></div>
-                                    <h7 className="card-title">{person.name}</h7>
+                                    <div><span style={{color: "red", fontSize: "8px"}}>شهید والامقام</span></div>
+                                    <h9 className="card-title" style={{fontSize: "12px"}}>{person.name}</h9>
                                 </div>
                             </div>
                         </div>
